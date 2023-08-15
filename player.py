28ec69014f1840560701359249f05f2ea1ec5e18
TimothyJNeale <119433369+TimothyJNeale@users.noreply.github.com>
2023-08-15 19:25:30 +0100

    Added buttoon image files

--- player.py
+++ player.py
@@ -1,8 +1,16 @@
 from tkinter import *
 
 root = Tk()
-root.title("Hello World!")
-root.geometry("500x500")
+root.title("MP3 Player")
+root.geometry("500x400")
 root.iconbitmap("01 Nenebiker.ico")
+
+
+playlist_box = Listbox(root, bg="black", fg="green", width=60, selectbackground="green", selectforeground="black")
+playlist_box.pack(pady=20)
+
+
+
+
 
 root.mainloop()
